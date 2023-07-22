df8b27f440c83d185aad0237ecd8c3f69073a044
Ayronman3 <sj.guevarra00275@student.tsu.edu.ph>
2023-07-23 00:37:12 +0800

    Finished Tutorial Vid but many errors in the console

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
 
+// API KEY: b11d47a
+
+const API_URL = 'http://www.omdbapi.com?apikey=b11d47a';
 const App = ()=>{
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const searchMovies = async(title)=>{
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  }
+  useEffect(()=>{
+    searchMovies('Ironman');
+  },[]);
   return(
-    <h1>App</h1>
+    <div  className="app">
+      <h1>MOVIEDB</h1>
+
+      <div className='search'>
+        <input
+        placeholder='Search for movie'
+        value={searchTerm}
+        onChange={(e)=>{setSearchTerm(e.target.value)}}
+        />
+        <img
+        src={SearchIcon}
+        alt='search'
+        onClick={()=>searchMovies(searchTerm)}/>
+      </div>
+      {
+        movies?.length > 0?(
+          <div className='container'>
+            {movies.map((movie)=>(
+              <MovieCard movie={movie}/>
+            ))}
+      </div>
+        ) :
+        (
+          <div className='empty'>
+            <h2>No Movies Found</h2>
+            </div>
+        )
+
+      };
+      
+    </div>
   )
 };
 
